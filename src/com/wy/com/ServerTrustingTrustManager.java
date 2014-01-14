a93aff78aef4df37692c3879375c9c67df5c1fea
Alex <alexemail7@126.com>
2014-01-14 17:57:51 +0800

    update ServerTrustingTrustManager

--- src/com/wy/com/ServerTrustingTrustManager.java
+++ src/com/wy/com/ServerTrustingTrustManager.java
@@ -1,8 +1,7 @@
-package com.wy.com;
+ package com.wy.com;
 
 import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
-
 import javax.net.ssl.X509TrustManager;
 
 public class ServerTrustingTrustManager implements X509TrustManager{
